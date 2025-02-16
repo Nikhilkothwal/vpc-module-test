c5382e178c415be45f203a9d8f740deeeefa53c3
nikhilkothwal <nkothwal@gmail.com>
2025-02-16 23:33:18 +0530

    vpc test module added

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 module "vpc_test" {
-    source = "../terraform-aws-vpc"
+    source = "git::https://github.com/Nikhilkothwal/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment = var.environment
     common_tags = var.common_tags
